bd15fa49ffa0398967758c1a4a5e2da6e7ff6aa7
Angus H Wright <awright@astro.rub.de>
2023-04-01 17:04:28 +0200

    k1000 rerun major push

---
+++ man/prepare_treecorr.man.sh
@@ -0,0 +1,79 @@
+#
+# prepare_treecorr.sh Documentation & Housekeeping functions
+#
+
+#Starting Prompt {{{
+function _prompt { 
+  #Check if we do want verbose output
+  if [ "$1" != "0" ] 
+  then
+    _message "@BLU@========================================@DEF@\n"
+    _message "@BLU@== @RED@ Running prepare_treecorr.sh Mode @BLU@ ==@DEF@\n"
+    _message "@BLU@========================================@DEF@\n"
+  fi 
+}
+#}}}
+
+#Mode description {{{
+function _description { 
+  echo "#"
+  echo '# Prepare the DATAHEAD catalogues for correlation '
+  echo '# function measurement using treecorr'
+  echo "#"
+  echo "# Function takes input data:"
+  echo "# `_inp_data`"
+  echo "#"
+}
+#}}}
+
+# Abort Message {{{
+_abort()
+{
+  #Message to print when script aborts 
+  #$0 is the script that was running when this error occurred
+  _message "@BLU@ An error occured while running:\n@DEF@$0.\n" >&2
+  _message "@BLU@ Check the logging file for this step in:\n" >&2
+  _message "@DEF@@RUNROOT@/@LOGPATH@/\n" >&2
+  exit 1
+}
+trap '_abort' 0
+set -e 
+#}}}
+
+# Input variables {{{ 
+function _inp_var { 
+  #Variable inputs (leave blank if none)
+  echo BLU DATABLOCK DECNAME DEF E1NAME E2NAME LOGPATH NBOOT PSFE1NAME PSFE2NAME PYTHON3BIN RANAME RED RUNROOT SCRIPTPATH STORAGEPATH WEIGHTNAME
+} 
+#}}}
+
+# Input data {{{ 
+function _inp_data { 
+  #Data inputs (leave blank if none)
+  echo DATAHEAD
+} 
+#}}}
+
+# Output data {{{ 
+function _outputs { 
+  #Data outputs (leave blank if none)
+  echo DATAHEAD
+} 
+#}}}
+
+# Execution command {{{ 
+function _runcommand { 
+  #Command for running the script 
+  echo bash @RUNROOT@/@SCRIPTPATH@/prepare_treecorr.sh
+} 
+#}}}
+
+# Unset Function command {{{ 
+function _unset_functions { 
+  #Remove these functions from the environment
+  unset -f _prompt _description _inp_data _inp_var _abort _outputs _runcommand _unset_functions
+} 
+#}}}
+
+#Additional Functions 
+
