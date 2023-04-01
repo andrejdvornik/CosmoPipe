c14669378a71119fa88fe45cb8c2d4e5080abf8b
Angus H Wright <awright@astro.rub.de>
2023-04-01 17:09:05 +0200

    add functions

---
+++ scripts/combine_xi_patches.R
@@ -0,0 +1,93 @@
+#=========================================
+#
+# File Name : combine_xi_patches.R
+# Created By : awright
+# Creation Date : 29-03-2023
+# Last Modified : Thu 30 Mar 2023 02:06:46 PM CEST
+#
+#=========================================
+
+#Get the input files 
+inputs<-commandArgs(TRUE) 
+
+#Interpret the command line options 
+while (length(inputs)!=0) {
+  while (length(inputs)!=0 && inputs[1]=='') { inputs<-inputs[-1] }  
+  if (!grepl('^-',inputs[1])) {
+    print(inputs)
+    stop(paste("Incorrect options provided!"))
+  }
+  #/*fend*/}}}
+  if (inputs[1]=='-i') { 
+    #Read the input catalogue(s) /*fold*/ {{{
+    inputs<-inputs[-1]
+    if (any(grepl('^-',inputs))) { 
+      input.cats<-inputs[1:(which(grepl('^-',inputs))[1]-1)]
+      inputs<-inputs[-(1:(which(grepl('^-',inputs))[1]-1))]
+    } else { 
+      input.cats<-inputs
+      inputs<-NULL
+    } 
+    #/*fold*/}}}
+  } else if (inputs[1]=='-o') { 
+    # Define the output file name {{{
+    inputs<-inputs[-1]
+    outputfile<-inputs[1]
+    inputs<-inputs[-1]
+    #/*fend*/}}}
+  } else {
+    stop(paste("Unknown option",inputs[1]))
+  }
+}
+
+colnames<-c('r_nom','meanr','meanlogr','xip','xim','xip_pm','xim_im','sigma_xip', 'npairs', 'weight','npairs_weighted')
+
+for (i in 1:length(input.cats)) { 
+  #Read the file 
+  tmp<-helpRfuncs::read.file(input.cats[i],data.table=FALSE)
+  #Add new values 
+  tmp$xim_im_sq <- tmp$xim_im^2
+  tmp$var_xip <- tmp$sigma_xip^2
+  tmp$npairs_sq <- tmp$npairs^2
+  
+  #if we have the first file, use this as a template 
+  if (i==1) { 
+    inter_cols<-c('r_nom','meanr','meanlogr','xip','xim','xip_pm','xim_im','xim_im_sq','var_xip', 'npairs_sq', 'weight','npairs_weighted')
+    #Set up the catalogue, but fill everything with zeros
+    out<-tmp[,inter_cols]*0
+  } 
+  print(summary(out))
+  #Otherwise, combine the values
+  #Weighted sum of values (weight = npairs_weighted)
+  out$r_nom     = (out$r_nom    + tmp$r_nom   * tmp$npairs_weighted)
+  out$meanr     = (out$meanr    + tmp$meanr   * tmp$npairs_weighted)
+  out$meanlogr  = (out$meanlogr + tmp$meanlogr* tmp$npairs_weighted)
+  out$xip       = (out$xip      + tmp$xip     * tmp$npairs_weighted)
+  out$xim       = (out$xim      + tmp$xim     * tmp$npairs_weighted)
+  out$xip_pm    = (out$xip_pm   + tmp$xip_pm  * tmp$npairs_weighted)
+  out$xim_im    = (out$xim_im   + tmp$xim_im  * tmp$npairs_weighted)
+  out$xim_im_sq = out$xim_im_sq + tmp$xim_im_sq 
+  out$var_xip = out$var_xip+tmp$var_xip 
+  out$npairs_sq = out$npairs_sq+tmp$npairs_sq 
+  out$weight = out$weight+tmp$weight
+  out$npairs_weighted = out$npairs_weighted+tmp$npairs_weighted
+}
+print(summary(out))
+#Construct the output columns 
+out$r_nom     = out$r_nom/out$npairs_weighted
+out$meanr     = out$meanr/out$npairs_weighted
+out$meanlogr  = out$meanlogr/out$npairs_weighted
+out$xip       = out$xip/out$npairs_weighted
+out$xim       = out$xim/out$npairs_weighted
+out$xip_pm    = out$xip_pm/out$npairs_weighted
+out$xim_im    = out$xim_im/out$npairs_weighted
+out$xim_im_sq = out$xim_im_sq + tmp$xim_im_sq 
+out$sigma_xip = sqrt(out$var_xip)
+out$npairs = sqrt(out$npairs_sq) 
+#Remove the intermediate columns and sort into the original order 
+out<-data.table::as.data.table(out)
+out<-out[,.SD,.SDcols=colnames]
+print(summary(out))
+
+#Output the file 
+helpRfuncs::write.file(outputfile,out)
---
+++ scripts/combine_xi_patches.sh
@@ -0,0 +1,83 @@
+#=========================================
+#
+# File Name : combine_xi_patches.sh
+# Created By : awright
+# Creation Date : 30-03-2023
+# Last Modified : Fri 31 Mar 2023 01:33:51 PM CEST
+#
+#=========================================
+
+### Combine the patch corrrelation functions ### {{{
+_message "Combining Correlation Functions by patch\n"
+headfiles="@DB:ALLHEAD@"
+
+ntomo=`_ntomo`
+#Loop over tomographic bins in this patch 
+for ZBIN1 in `seq ${ntomo}`
+do
+  #Define the Z_B limits from the TOMOLIMS {{{
+  ZB_lo=`echo @TOMOLIMS@ | awk -v n=$ZBIN1 '{print $n}'`
+  ZB_hi=`echo @TOMOLIMS@ | awk -v n=$ZBIN1 '{print $(n+1)}'`
+  #}}}
+  #Define the string to append to the file names {{{
+  ZB_lo_str=`echo $ZB_lo | sed 's/\./p/g'`
+  ZB_hi_str=`echo $ZB_hi | sed 's/\./p/g'`
+  appendstr="_ZB${ZB_lo_str}t${ZB_hi_str}"
+  #}}}
+  
+  for ZBIN2 in `seq $ZBIN1 ${ntomo}`
+  do
+    ZB_lo2=`echo @TOMOLIMS@ | awk -v n=$ZBIN2 '{print $n}'`
+    ZB_hi2=`echo @TOMOLIMS@ | awk -v n=$ZBIN2 '{print $(n+1)}'`
+    ZB_lo_str2=`echo $ZB_lo2 | sed 's/\./p/g'`
+    ZB_hi_str2=`echo $ZB_hi2 | sed 's/\./p/g'`
+    appendstr2="_ZB${ZB_lo_str2}t${ZB_hi_str2}"
+
+    #Define the input file id 
+    filestr="${appendstr}${appendstr2}_ggcorr.txt"
+
+    #Get the file list {{{
+    filelist=''
+    for patch in @PATCHLIST@ 
+    do 
+      file=`echo ${headfiles} | sed 's/ /\n/g' | grep "_${patch}_" | grep ${filestr} || echo `
+      #Check if the output file exists 
+      if [ "${file}" == "" ] 
+      then 
+        _message "@RED@ - ERROR!\n"
+        _message "A file with the ID string @DEF@${filestr}@RED@ in patch @DEF@${patch}@RED@ does not exist in the data head\n"
+        exit 1 
+      fi 
+      #Add the file to the file list 
+      filelist="${filelist} ${file}"
+
+      #Define the output name 
+      outname=${file//_${patch}_/_@ALLPATCH@comb_}
+    done 
+    #}}}
+
+    #Combine the patches for this bin 
+    _message "    -> @BLU@Bin $ZBIN1 ($ZB_lo < Z_B <= $ZB_hi) x Bin $ZBIN2 ($ZB_lo2 < Z_B <= $ZB_hi2)@DEF@"
+    logfile=${outname##*/}
+    @P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/combine_xi_patches.R \
+      -o ${outname} \
+      -i ${filelist} \
+      > @RUNROOT@/@LOGPATH@/${logfile//.txt/.log} 2>&1 
+    _message " - @RED@Done!@DEF@\n"
+    #Add the new file to the datahead 
+    count=0
+    for file in ${filelist} 
+    do 
+      if [ $count -gt 0 ]
+      then 
+        #Don't replace with anything 
+        _replace_datahead "${file##*/}" ""
+      else 
+        #Replace with new file 
+        _replace_datahead "${file##*/}" "${outname##*/}"
+        count=1
+      fi 
+    done 
+  done
+done
+_message "  }\n"
