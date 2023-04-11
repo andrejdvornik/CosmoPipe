7dd438fddd58a23dcdf2c03fae6d8c8db3ec12f8
Jan Luca van den Busch <jlvdb@astro.rub.de>
2023-04-11 16:39:20 +0000

    fixed downgrading of numpy version to 1.20 during installation

--- COSMOPIPE_MASTER_INSTALL.sh
+++ COSMOPIPE_MASTER_INSTALL.sh
@@ -400,7 +400,7 @@ EOF
 ${RUNROOT}/INSTALL/miniconda3/bin/conda install mpich-mpicc mpi4py >> python_packages.log 2>&1 <<EOF
 y
 EOF
-${RUNROOT}/INSTALL/miniconda3/bin/pip install emcee numpy==1.20.0 scipy pyfits cython matplotlib \
+${RUNROOT}/INSTALL/miniconda3/bin/pip install emcee numpy==1.23.1 scipy pyfits cython matplotlib \
   palettable fitsio==1.1.1 astropy >> python_packages.log 2>&1 <<EOF
 y
 EOF
