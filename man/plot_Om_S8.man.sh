0a45a4d63f761ecf718d068708554731c13a72b0
Angus H Wright <awright@astro.rub.de>
2023-05-04 23:43:06 +0200

    add plot OmS8 doc

---
+++ man/plot_Om_S8.man.sh
@@ -0,0 +1,79 @@
+#
+# plot_Om_S8.sh Documentation & Housekeeping functions
+#
+
+#Starting Prompt {{{
+function _prompt { 
+  #Check if we do want verbose output
+  if [ "$1" != "0" ] 
+  then
+    _message "@BLU@==================================@DEF@\n"
+    _message "@BLU@== @RED@ Running plot_Om_S8.sh Mode @BLU@ ==@DEF@\n"
+    _message "@BLU@==================================@DEF@\n"
+  fi 
+}
+#}}}
+
+#Mode description {{{
+function _description { 
+  echo "#"
+  echo '# Plot an Omega_m vs S8 contour from the chain '
+  echo '# outputs'
+  echo "#"
+  echo "# Function takes input data:"
+  echo "# `_inp_data`"
+  echo "#"
+}
+#}}}
+
+# Abort Message {{{
+_abort()
+{
+  #Message to print when script aborts 
+  #$0 is the script that was running when this error occurred
+  _message "@BLU@ An error occured while running:\n@DEF@$0.\n" >&2
+  _message "@BLU@ Check the logging file for this step in:\n" >&2
+  _message "@DEF@@RUNROOT@/@LOGPATH@/\n" >&2
+  exit 1
+}
+trap '_abort' 0
+set -e 
+#}}}
+
+# Input variables {{{ 
+function _inp_var { 
+  #Variable inputs (leave blank if none)
+  echo BLINDING P_RSCRIPT RUNROOT SCRIPTPATH STORAGEPATH SURVEY DB:BLIND DB:BOLTZMAN DB:REFCHAIN DB:SAMPLER DB:STATISTIC
+} 
+#}}}
+
+# Input data {{{ 
+function _inp_data { 
+  #Data inputs (leave blank if none)
+  echo 
+} 
+#}}}
+
+# Output data {{{ 
+function _outputs { 
+  #Data outputs (leave blank if none)
+  echo ""
+} 
+#}}}
+
+# Execution command {{{ 
+function _runcommand { 
+  #Command for running the script 
+  echo bash @RUNROOT@/@SCRIPTPATH@/plot_Om_S8.sh
+} 
+#}}}
+
+# Unset Function command {{{ 
+function _unset_functions { 
+  #Remove these functions from the environment
+  unset -f _prompt _description _inp_data _inp_var _abort _outputs _runcommand _unset_functions
+} 
+#}}}
+
+#Additional Functions 
+
