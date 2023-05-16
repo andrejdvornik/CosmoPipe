c3473be3ee916879fcf8e73136f6f84f6f711d55
Angus H Wright <awright@astro.rub.de>
2023-05-16 06:46:55 +0200

    bug fix cleanfits

--- scripts/cleanfits.R
+++ scripts/cleanfits.R
@@ -3,7 +3,7 @@
 # File Name : cleanfits.R
 # Created By : awright
 # Creation Date : 09-05-2023
-# Last Modified : Mon 15 May 2023 11:11:50 AM CEST
+# Last Modified : Tue 16 May 2023 06:45:13 AM CEST
 #
 #=========================================
 
@@ -15,12 +15,12 @@ input<-commandArgs(T)[1]
 cat<-Rfits::Rfits_read_table(input,verbose=T,nrow=1e3)
 #Convert integer64 into integer (not handled by ldac) 
 classes<-unlist(lapply(cat,class))
+include_fields=TRUE
 if (any(classes=="integer64")) {
   cat<-Rfits::Rfits_read_table(input,verbose=T)
   for (i in names(classes)[which(classes=='integer64')]) {
     cat[[i]]<-as.integer(cat[[i]])
   }
-  include_fields=TRUE
   if (include_fields) {
     #Construct the output file 
     fields<-data.frame(OBJECT_POS=as.integer(1),OBJECT_COUNT=nrow(cat),CHANNEL_NR=as.integer(0),CHANNEL_NAME="I",SubNr=as.integer(1))
@@ -36,4 +36,13 @@ if (any(classes=="integer64")) {
   } else { 
     Rfits::Rfits_write_table(file=input,cat,verbose=T)
   } 
+} else if (include_fields) { 
+  fields<-data.frame(OBJECT_POS=as.integer(1),OBJECT_COUNT=nrow(cat),CHANNEL_NR=as.integer(0),CHANNEL_NAME="I",SubNr=as.integer(1))
+  extn<-Rfits::Rfits_extnames(filename=input)
+  if (all(extn!="OBJECTS",na.rm=T)) { 
+    Rfits::Rfits_write_key(file=input,"EXTNAME","OBJECTS","",ext=length(extn))
+  } 
+  Rfits::Rfits_write_table(file=input,fields,verbose=T,extname="FIELDS",overwrite_file=FALSE,create_file=FALSE,ext=length(extn)+1)
 }
+
+
