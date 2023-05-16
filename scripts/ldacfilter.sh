63760de86af4bfb99137cbf4dbaf626cbad91005
Angus H Wright <awright@astro.rub.de>
2023-05-16 15:09:43 +0200

    formatting updates

--- scripts/ldacfilter.sh
+++ scripts/ldacfilter.sh
@@ -3,7 +3,7 @@
 # File Name : ldacfilter.sh
 # Created By : awright
 # Creation Date : 16-05-2023
-# Last Modified : Tue 16 May 2023 02:44:41 PM CEST
+# Last Modified : Tue 16 May 2023 03:09:34 PM CEST
 #
 #=========================================
 
@@ -13,14 +13,14 @@ ext=${input##*.}
 
 #Filter the DATAHEAD catalogues based on the block-variable condition
 _message "@BLU@Using Condition @DEF@@BV:FILTERCOND@@DEF@\n"
-_message "@BLU@Creating Filtered catalogue for @DEF@${input##*/}@DEF@\n"
+_message "@BLU@Creating Filtered catalogue for @DEF@${input##*/}@DEF@"
 #Select sources using required filter condition 
 @PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/ldacfilter.py \
   -i ${input} \
   -t OBJECTS \
   -c "(@BV:FILTERCOND@);" \
   -o ${input//.${ext}/_filt.${ext}} 2>&1 
-_message " - @RED@Done! (`date +'%a %H:%M'`)@DEF@\n"
+_message " @BLU@- @RED@Done! (`date +'%a %H:%M'`)@DEF@\n"
 output=${input//.cat/_filt.cat}
 _replace_datahead ${input##*/} ${output##*/}
 
