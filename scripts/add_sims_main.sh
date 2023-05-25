8ffa10cb0af97b4d67b59922c487e611e1691dbe
Angus H Wright <awright@astro.rub.de>
2023-05-25 23:49:30 +0200

    cleanup add_sims_main

--- scripts/add_sims_main.sh
+++ scripts/add_sims_main.sh
@@ -2,17 +2,7 @@
 # Add the calibration sample to the data block 
 #
 
-##Check that the simulated catalogue exists
-#if [ ! -f @BV:SIMMAINCAT@ ]
-#then 
-#  _message "${RED} - ERROR: simulated main catalogue @BV:SIMMAINCAT@ does not exist!"
-#  exit -1 
-#fi 
-#
-##Update the datablock contents file 
-#_add_datablock sims_main @BV:SIMMAINCAT@
-
-#If needed, make the gold catalogue folder 
+#If needed, make the simulations folder 
 if [ ! -d @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/sims_main/ ]
 then 
   mkdir @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/sims_main
