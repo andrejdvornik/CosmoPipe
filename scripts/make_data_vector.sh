fc034d7375d5464ba4754cdf93b0aba9a21c08b3
Angus H Wright <awright@astro.rub.de>
2023-06-13 10:22:37 +0200

    clean up make_data_vector

--- scripts/make_data_vector.sh
+++ scripts/make_data_vector.sh
@@ -3,48 +3,46 @@
 # File Name : make_data_vector.sh
 # Created By : awright
 # Creation Date : 01-04-2023
-# Last Modified : Thu 01 Jun 2023 03:48:15 PM CEST
+# Last Modified : Thu 01 Jun 2023 06:02:15 PM CEST
 #
 #=========================================
 
-#Select the first file (contains the mbias values)
-mbias=`echo @DB:mbias@ | awk '{print $1}'`
-#Get the actual m values from the file (ignore any header)
-mbias="`cat ${mbias} | grep -v "^#"`"
-
+#Input data vectors 
 inputs="@DB:cosebis@"
+#Input mbias files 
+mfiles="@DB:mbias@"
 
 for patch in @PATCHLIST@ @ALLPATCH@
 do 
-  filelist=''
+  #Get the input files for this patch (there should be NTOMO catalogues)
+  filelist=`echo ${inputs} | sed 's/ /\n/g' | grep "_${patch}_" || echo `
+  #Get the m-bias file for this patch (there should be one, with NTOMO entries)
+  biaslist=`echo ${mfiles} | sed 's/ /\n/g' | grep "_${patch}_" | grep "_biases" || echo `
 
-  file=`echo ${inputs} | sed 's/ /\n/g' | grep "_${patch}_" || echo `
-  #Check if the output file exists {{{
-  if [ "${file}" == "" ] 
+  #Check if there are any matching files {{{
+  if [ "${filelist}" == "" ] 
   then 
     #If not, loop
     continue
   fi 
   #}}}
-  filelist="${filelist} ${file}"
-  #If filelist is empty, skip {{{
-  if [ "${filelist}" == "" ] 
-  then 
-    continue
-  fi 
 
-  #If needed, create the output directory 
+  #If needed, create the output directory {{{
   if [ ! -d @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/cosebis_vec_${patch} ]
   then 
     mkdir @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/cosebis_vec_${patch}/
   fi 
+  #}}}
 
-  #Construct the data vector for cosebis
+  #Construct the data vector for cosebis {{{
   @PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/make_data_vector.py \
     --inputfiles ${filelist}   \
-    --mbias   ${mbias}      \
+    --mbias   ${biaslist}      \
     --tomobins @BV:TOMOLIMS@  \
     --outputfile  @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/cosebis_vec_${patch}/combined_vector.txt 
+  #}}}
 
+  #Update the datablock 
   _write_datablock "cosebis_vec_${patch}" "combined_vector.txt"
 done
+
