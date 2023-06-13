53f874e7c4a2059267e13bb919be8188bd0f3d7f
Angus H Wright <awright@astro.rub.de>
2023-06-13 10:20:11 +0200

    only update the catalogue if it's not already LDAC

--- scripts/fits_str_col_fix.py
+++ scripts/fits_str_col_fix.py
@@ -7,14 +7,26 @@ Signature: fits_str_col_fix.py $infile $outfile
 import sys
 
 import astropandas as apd
-
+import mcal_functions as mcf
+import pandas as pd
 
 if len(sys.argv) != 3:
     raise ValueError("input and output file paths are required arguments")
 
-data = apd.read_fits(sys.argv[1])
-for col, dtype in zip(data.columns, data.dtypes):
-    if dtype.kind == "O":  # string columns are represented as python objects
-        data[col] = data[col].astype("|S")  # required minimum width is determined automatically
-        print("converted column '{:}' to data type '{:}'".format(col, data[col].dtype.str))
-apd.to_fits(data, sys.argv[2])
+modified=False
+data, ldac_cat = mcf.flexible_read(sys.argv[1],as_df=True)
+
+#If the data was read as a data frame (and so is not LDAC)
+if isinstance(data, pd.DataFrame):
+    #Check for bad columns 
+    for col, dtype in zip(data.columns, data.dtypes):
+        #Convert if needed
+        if dtype.kind == "O":  # string columns are represented as python objects
+            data[col] = data[col].astype("|S")  # required minimum width is determined automatically
+            print("converted column '{:}' to data type '{:}'".format(col, data[col].dtype.str))
+            modified=True
+    
+    #Write out the new catalogue (if needed) 
+    if modified: 
+        apd.to_fits(data, sys.argv[2])
+
