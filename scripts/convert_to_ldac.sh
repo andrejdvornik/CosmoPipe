86d5f0f2195a8cb11e4b496d44366538a6761cb5
Angus H Wright <awright@astro.rub.de>
2023-06-13 10:18:42 +0200

    update convert_to_ldac for better performance and functionality

--- scripts/convert_to_ldac.sh
+++ scripts/convert_to_ldac.sh
@@ -3,20 +3,33 @@
 # File Name : convert_to_ldac.sh
 # Created By : awright
 # Creation Date : 20-03-2023
-# Last Modified : Fri 02 Jun 2023 01:20:51 PM CEST
+# Last Modified : Fri 02 Jun 2023 10:35:47 PM CEST
 #
 #=========================================
 
 #Input file name 
 input="@DB:DATAHEAD@"
 
-#Notify 
-_message "   > @BLU@Cleaning FITS catalogue for use as LDAC: @DEF@${input##*/}@DEF@ "
+objstr=''
+echo "Testing existance of OBJECTS, FIELDS, FIELD_POS, and SeqNr"
+@RUNROOT@/INSTALL/theli-1.6.1/bin/@MACHINE@/ldactestexist -i ${input} -t OBJECTS -k FIELD_POS 2>&1 || objstr="FAIL"
+@RUNROOT@/INSTALL/theli-1.6.1/bin/@MACHINE@/ldactestexist -i ${input} -t OBJECTS -k SeqNr 2>&1 || objstr="FAIL"
+@RUNROOT@/INSTALL/theli-1.6.1/bin/@MACHINE@/ldactestexist -i ${input} -t FIELDS 2>&1 || objstr="FAIL"
 
-#Clean the first catalogue of variable-length items 
-@PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/fits_str_col_fix.py ${input} ${input} 2>&1
+if [ "${objstr}" == "FAIL" ] 
+then 
+  #Notify 
+  _message "   > @BLU@Converting non-LDAC FITS catalogue for use as LDAC: @DEF@${input##*/}@DEF@ "
 
-#Convert the catalogue int64 columns to int32
-@P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/convert_to_ldac.R ${input} 2>&1
+  #Clean the first catalogue of variable-length items 
+  @PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/fits_str_col_fix.py ${input} ${input} 2>&1
 
-_message " @BLU@- @RED@Done! (`date +'%a %H:%M'`)\n@DEF@"
+  #Convert the catalogue int64 columns to int32
+  @P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/convert_to_ldac.R ${input} 2>&1
+
+  _message " @BLU@- @RED@Done! (`date +'%a %H:%M'`)\n@DEF@"
+else 
+  #Notify 
+  _message "   > @BLU@Catalogue @DEF@${input##*/}@BLU@ already appears LDAC compatible!@DEF@\n"
+fi 
+
