211c951f32fc5349f4abff43beffb0a6966054bd
Angus H Wright <awright@astro.rub.de>
2023-06-13 10:27:11 +0200

    don't write to screen in adapt creation

--- scripts/use_adapt_mag.sh
+++ scripts/use_adapt_mag.sh
@@ -10,7 +10,7 @@ orig=${orig##*/}
 _message "  > @BLU@Constructing Adapt catalogue for file: @DEF@${orig}\n"
 
 #Construct the adapt catalogue 
-@P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/construct_adapt_catalogue.R @DB:DATAHEAD@ @DB:DATAHEAD@
+@P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/construct_adapt_catalogue.R @DB:DATAHEAD@ @DB:DATAHEAD@ 2>&1
 
 #The script forcibly renames .cat files to .fits
 #Make sure that this is reflected in the DATAHEAD 
