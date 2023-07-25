e68271138852f1a817fd0db1da9fc63ab295e5a4
Angus H Wright <awright@astro.rub.de>
2023-07-25 15:44:32 +0200

    convert output extension to .cat, and add catches for various usage states

--- scripts/convert_to_ldac.sh
+++ scripts/convert_to_ldac.sh
@@ -3,12 +3,14 @@
 # File Name : convert_to_ldac.sh
 # Created By : awright
 # Creation Date : 20-03-2023
-# Last Modified : Tue 13 Jun 2023 04:06:54 PM CEST
+# Last Modified : Tue 25 Jul 2023 03:38:34 PM CEST
 #
 #=========================================
 
 #Input file name 
 input="@DB:DATAHEAD@"
+extn=${input##*.}
+output=${input%.${extn}}.cat
 
 objstr=''
 _message "@BLU@Testing existance of @RED@OBJECTS@BLU@, @RED@FIELDS@BLU@, @RED@FIELD_POS@BLU@, and @RED@SeqNr@BLU@:\n{@DEF@\n"
@@ -22,15 +24,35 @@ then
   #Notify 
   _message "   > @BLU@Converting non-LDAC FITS catalogue for use as LDAC: @DEF@${input##*/}@DEF@ "
 
-  #Clean the first catalogue of variable-length items 
-  @PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/fits_str_col_fix.py ${input} ${input} 2>&1
+  #Remove existing output file 
+  if [ "${input}" != "${output}" ] && [ -f ${output} ]
+  then 
+    rm ${output}
+  fi 
 
-  #Convert the catalogue int64 columns to int32
-  @P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/convert_to_ldac.R ${input} 2>&1
+  #Clean the first catalogue of variable-length items 
+  @PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/fits_str_col_fix.py ${input} ${output} 2>&1
+
+  #If the output file doesn't exist, nothing was updated; duplicate the input 
+  if [ ! -f ${output} ]
+  then 
+    rsync -atvL ${input} ${output}
+  fi 
+
+  #Convert the catalogue int64 columns to int32, and enforce LDAC format
+  @P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/convert_to_ldac.R ${output} 2>&1
 
   _message " @BLU@- @RED@Done! (`date +'%a %H:%M'`)\n@DEF@"
+
+  #Update the datahead 
+  _replace_datahead ${input##*/} ${output##*/}
 else 
   #Notify 
   _message "   > @BLU@Catalogue @DEF@${input##*/}@BLU@ already appears LDAC compatible!@DEF@\n"
+  #Check if we need to rename 
+  if [ "${input}" != "${output}" ]
+  then 
+    _replace_datahead ${input##*/} ${output##*/}
+  fi 
 fi 
 
