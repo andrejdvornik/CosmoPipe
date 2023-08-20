63cde02ea904fbd6501c3abbc3e4ebaf04387a56
Angus H Wright <awright@astro.rub.de>
2023-08-20 22:50:52 +0200

    add inherits function

---
+++ scripts/inherit.sh
@@ -0,0 +1,59 @@
+#=========================================
+#
+# File Name : inherit.sh
+# Created By : awright
+# Creation Date : 20-08-2023
+# Last Modified : Sun 20 Aug 2023 10:21:09 PM CEST
+#
+#=========================================
+
+#Get the inheritence pipeline name 
+pipename="@BV:INHERIT_PIPE@"
+
+#Get the inheritence pipeline block element 
+blocklist="@BV:INHERIT_BLOCK@"
+
+#Construct the inheritence path 
+pipepath="@RUNROOT@/@STORAGEPATH@/@DATABLOCK@/"
+pipepath=${pipepath/@PIPELINE@/${pipename}}
+
+#For each block requested 
+for block in ${blocklist}
+do 
+  #Test that the item exists 
+  if [ ! -d ${pipepath}/${block} ] 
+  then 
+    _message "@RED@ - ERROR! Inheritence pipeline block element does not exist!"
+    exit 1 
+  fi 
+
+  #Test that the target item does not exist
+  if [  -d @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/${block} ] 
+  then 
+    _message "@RED@ - ERROR! Inheritence would overwrite existing block element!" 
+    exit 1 
+  fi 
+
+  #Notify
+  _message " > @BLU@Inheriting block item @DEF@${block}@BLU@ from pipeline @DEF@${pipename}"
+  
+  #Make the new block element 
+  mkdir @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/${block}
+  
+  #Loop through the new files 
+  filelist=''
+  for file in `ls @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/${block}` 
+  do 
+    #Add the new file to the filelist 
+    filelist="${filelist} ${file##*/}"
+    #Inherit! 
+    rsync -atvL ${pipepath}/${file##*/} @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/${block}/${file##*/}
+  done 
+  
+  #Update the datablock 
+  _write_datablock "${block}" "${filelist}"
+
+  #Notify 
+  _message " - @RED@Done! (`date +'%a %H:%M'`)@DEF@\n"
+done 
+
