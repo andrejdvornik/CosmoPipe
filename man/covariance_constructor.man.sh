07e814a1731562ecb1b3305f43ef7c677c21bd75
BStoelzner <stoelzner@astro.rub.de>
2023-09-04 08:06:55 +0000

    add missing input

--- man/covariance_constructor.man.sh
+++ man/covariance_constructor.man.sh
@@ -49,7 +49,7 @@ function _inp_var {
 # Input data {{{ 
 function _inp_data { 
   #Data inputs (leave blank if none)
-  echo cosmosis_neff cosmosis_nz cosmosis_sigmae
+  echo cosmosis_neff cosmosis_nz cosmosis_sigmae cosmosis_mbias
 } 
 #}}}
 
