8c704908349134b084442a3561839d9843dade8e
BStoelzner <stoelzner@astro.rub.de>
2023-09-05 10:01:20 +0000

    use correct nxipm

--- scripts/rebin_xipm.sh
+++ scripts/rebin_xipm.sh
@@ -35,9 +35,9 @@ _message "    -> @BLU@Rebinning xipm for file @RED@${input##*/}@DEF@"
   -i ${input} \
   -t "meanr" -p "xip" -m "xim" \
   --cfoldername ${outfold} \
-  -o ${output} -b @BINNING@ --nbins_bp @BV:NXIPM@ \
+  -o ${output} -b @BINNING@ --nbins_xipm @BV:NXIPM@ \
   -s @BV:THETAMINXI@ -l @BV:THETAMAXXI@  \
-  -d "xipm" 2>&1 
+ -d "xipm" 2>&1 
 _message " - @RED@Done! (`date +'%a %H:%M'`)@DEF@\n"
 
 #Add the files to the datablock 
