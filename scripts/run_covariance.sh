2ab21a347841e93c3c2d27192aa8e84d557e5156
BStoelzner <stoelzner@astro.rub.de>
2023-09-08 13:42:29 +0000

    set OMP_NUM_THREADS in covariance calculation

--- scripts/run_covariance.sh
+++ scripts/run_covariance.sh
@@ -15,7 +15,7 @@ fi
 
 #Run cosmosis for a constructed ini file 
 _message " >@BLU@ Running covariance!\n   Start time:@DEF@ `date +'%a %H:%M'`@BLU@)\n@DEF@"
-MKL_NUM_THREADS=1 NUMEXPR_NUM_THREADS=1 OMP_NUM_THREADS=32 @PYTHON3BIN@ @RUNROOT@/INSTALL/OneCovariance/covariance.py @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/covariance_inputs/@BV:STATISTIC@_@SURVEY@_CosmoPipe_constructed.ini 2>&1 
+MKL_NUM_THREADS=1 NUMEXPR_NUM_THREADS=1 OMP_NUM_THREADS=@BV:NTHREADS@ @PYTHON3BIN@ @RUNROOT@/INSTALL/OneCovariance/covariance.py @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/covariance_inputs/@BV:STATISTIC@_@SURVEY@_CosmoPipe_constructed.ini 2>&1 
 _message " >@RED@ Done! (`date +'%a %H:%M'`)@DEF@\n"
 
 _write_datablock "covariance_@BV:STATISTIC@" "covariance_matrix.mat"
