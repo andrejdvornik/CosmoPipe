44ad92739b13bf812577c53c91aaa9a07bc2abf5
Angus H Wright <awright@astro.rub.de>
2023-10-15 11:58:45 +0200

    update save_and_check_mcmc_inp for new output structure

--- scripts/save_and_check_mcmc_inp.sh
+++ scripts/save_and_check_mcmc_inp.sh
@@ -3,14 +3,14 @@
 # File Name : save_and_check_mcmc_inp.sh
 # Created By : awright
 # Creation Date : 01-04-2023
-# Last Modified : Fri 07 Jul 2023 08:10:16 PM CEST
+# Last Modified : Thu 07 Sep 2023 07:08:45 PM UTC
 #
 #=========================================
 
 #If needed, create the output directory 
-if [ ! -d @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mcmc_inp ]
+if [ ! -d @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mcmc_inp_cosebis ]
 then 
-  mkdir @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mcmc_inp/
+  mkdir @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mcmc_inp_cosebis/
 fi 
 if [ ! -d @RUNROOT@/@STORAGEPATH@/MCMC/input/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/plots ]
 then 
@@ -32,7 +32,7 @@ NTOMO=`echo @BV:TOMOLIMS@ | awk '{print NF-1}'`
   --neff @DB:cosmosis_neff@ \
   --sigmae @DB:cosmosis_sigmae@ \
   --covariance @DB:cosebis_cov@ \
-  --outputfile @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mcmc_inp/MCMC_input_${cov} \
+  --outputfile @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mcmc_inp_cosebis/MCMC_input_${cov} \
   --plotfolder @RUNROOT@/@STORAGEPATH@/MCMC/input/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/plots/
 
-_write_datablock "mcmc_inp" "MCMC_input_${cov}"
+_write_datablock "mcmc_inp_cosebis" "MCMC_input_${cov}"
