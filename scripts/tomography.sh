9a480245d8b6cfc1fba11868a42ed72e864ca918
Angus H Wright <awright@astro.rub.de>
2023-11-03 13:38:28 +0100

    stop tomography printing shell messages

--- scripts/tomography.sh
+++ scripts/tomography.sh
@@ -41,14 +41,76 @@ do
     _message " @RED@- Done! (`date +'%a %H:%M'`)@DEF@\n"
   fi 
   #}}}
+  #Check if input file lengths are ok {{{
+  links="FALSE"
+  for file in ${inputfile} ${outputname}
+  do 
+    if [ ${#file} -gt 255 ] 
+    then 
+      links="TRUE"
+    fi 
+  done 
+  
+  if [ "${links}" == "TRUE" ] 
+  then
+    #Remove existing infile links 
+    if [ -e infile.lnk ] || [ -h infile.lnk ]
+    then 
+      rm infile.lnk
+    fi 
+    #Remove existing outfile links 
+    if [ -e outfile.lnk ] || [ -h outfile.lnk ]
+    then 
+      rm outfile.lnk
+    fi
+    #Create input link
+    originp=${inputfile}
+    ln -s ${inputfile} infile.lnk 
+    inputfile="infile.lnk"
+    #Create output links 
+    ln -s ${outputname} outfile.lnk
+    origout=${outputname}
+    outputname=outfile.lnk
+  fi 
+  #}}}
+
   #Construct the output tomographic bin {{{
   _message "   > @BLU@Constructing catalogue for tomographic bin ${i}@DEF@ ($TV_lo < @BV:TOMOVAR@ <= $TV_hi)"
-  @PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/ldacfilter.py \
-           -i ${inputfile} \
-  	       -o ${outputname} \
-  	       -t OBJECTS \
-  	       -c "(@BV:TOMOVAR@>${TV_lo})AND(@BV:TOMOVAR@<=${TV_hi});" 2>&1
-  _message " @RED@- Done! (`date +'%a %H:%M'`)@DEF@\n"
+  #Select sources using required tomographic filter condition 
+  ldacpass=TRUE
+  {
+  @RUNROOT@/INSTALL/theli-1.6.1/bin/@MACHINE@/ldacfilter \
+    -i ${inputfile} \
+  	-o ${outputname} \
+  	-t OBJECTS \
+    -c "((@BV:TOMOVAR@>${TV_lo})AND(@BV:TOMOVAR@<=${TV_hi}));" 2>&1 || ldacpass=FALSE
+  } >&1
+  #}}}
+
+  #If used, undo the links {{{
+  if [ "${links}" == "TRUE" ] 
+  then 
+    rm ${inputfile} ${outputname}
+    inputfile=${originp}
+    outputname=${origout}
+  fi 
+  #}}}
+
+  #Check if ldac failed {{{
+  if [ "${ldacpass}" == "TRUE" ]
+  then 
+    _message " @BLU@- @RED@Done! (`date +'%a %H:%M'`)@DEF@\n"
+  else 
+    #If so, run with python {{{
+    _message " @BLU@(py)@DEF@"
+    @PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/ldacfilter.py \
+             -i ${inputfile} \
+    	       -o ${outputname} \
+    	       -t OBJECTS \
+    	       -c "(@BV:TOMOVAR@>${TV_lo})AND(@BV:TOMOVAR@<=${TV_hi});" 2>&1
+    _message " @BLU@- @RED@Done! (`date +'%a %H:%M'`)@DEF@\n"
+    #}}}
+  fi 
   #}}}
 done
 #}}}
