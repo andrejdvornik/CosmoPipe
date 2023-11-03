0a083a64a99967209ad1dd18859c631a483eab09
BStoelzner <stoelzner@astro.rub.de>
2023-11-03 13:08:35 +0000

    do not use mpi with test sampler

--- scripts/run_chain.sh
+++ scripts/run_chain.sh
@@ -12,7 +12,7 @@ ppython=@PYTHON3BIN@
 pythonbin=${ppython%/*}
 _message " >@BLU@ Running cosmosis chain!\n   Start time:@DEF@ `date +'%a %H:%M'`@BLU@)\n@DEF@"
 _message " >@BLU@ Status can be monitored in the logfile located here:\n@RED@ `ls -tr @RUNROOT@/@LOGPATH@/step_*_run_chain.log | tail -n 1` @DEF@\n"
-if [ @BV:NTHREADS@ -eq 1 ]
+if [ @BV:NTHREADS@ -eq 1 ] || [ "@BV:SAMPLER@" == "test" ]
 then
 MKL_NUM_THREADS=1 NUMEXPR_NUM_THREADS=1 OMP_NUM_THREADS=1 ${pythonbin}/cosmosis @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/cosmosis_inputs/@SURVEY@_CosmoPipe_constructed_@BV:STATISTIC@.ini 2>&1
 else
