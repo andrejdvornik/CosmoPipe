892aed8090de5979e8bc33a7dccfba62209bb1c3
Angus H Wright <awright@astro.rub.de>
2023-11-03 11:45:47 +0100

    stop ldacfilter printing shell messages on failure

--- scripts/ldacfilter.sh
+++ scripts/ldacfilter.sh
@@ -3,7 +3,7 @@
 # File Name : ldacfilter.sh
 # Created By : awright
 # Creation Date : 16-05-2023
-# Last Modified : Tue 04 Jul 2023 09:24:49 PM CEST
+# Last Modified : Fri 03 Nov 2023 11:45:30 AM CET
 #
 #=========================================
 
@@ -37,14 +37,96 @@ fi
 
 #Filter the DATAHEAD catalogues based on the block-variable condition
 _message "@BLU@Using Condition @DEF@@BV:FILTERCOND@@DEF@\n"
+
+#Check if input file lengths are ok 
+links="FALSE"
+for file in ${input} ${outname}
+do 
+  if [ ${#file} -gt 255 ] 
+  then 
+    links="TRUE"
+  fi 
+done 
+
+if [ "${links}" == "TRUE" ] 
+then
+  #Remove existing infile links 
+  if [ -e infile.lnk ] || [ -h infile.lnk ]
+  then 
+    rm infile.lnk
+  fi 
+  #Remove existing outfile links 
+  if [ -e outfile.lnk ] || [ -h outfile.lnk ]
+  then 
+    rm outfile.lnk
+  fi
+  #Create input link
+  originp=${input}
+  ln -s ${input} infile.lnk 
+  input="infile.lnk"
+  #Create output links 
+  ln -s ${output} outfile.lnk
+  origout=${output}
+  output=outfile.lnk
+fi 
+
+#Try to filter with ldactools 
 _message "@BLU@Creating Filtered catalogue for @DEF@${input##*/}@DEF@"
+#Filter condition for ldactools 
+filtercond="@BV:FILTERCOND@"
+count=0
+while [[ "${filtercond}" =~ "&" ]]
+do 
+  count=$((count+1))
+  filtercond="(${filtercond}"
+  filtercond=${filtercond/&/AND}
+  start=${filtercond%%&*}
+  ending="${filtercond#*&}"
+  if [ "${ending}" == "${filtercond}" ]
+  then 
+    filtercond="${start})"
+  else 
+    filtercond="${start})&${ending}"
+  fi 
+  if [ ${count} -gt 100 ] 
+  then 
+    _message "ERROR IN ldac filter command construction!"
+    exit 1
+  fi 
+done 
+filtercond=${filtercond//==/=}
+ldacpass=TRUE
 #Select sources using required filter condition 
-@PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/ldacfilter.py \
+{ 
+@RUNROOT@/INSTALL/theli-1.6.1/bin/@MACHINE@/ldacfilter \
   -i ${input} \
   -t OBJECTS \
-  -c "(@BV:FILTERCOND@);" \
-  -o ${output} 2>&1 
-_message " @BLU@- @RED@Done! (`date +'%a %H:%M'`)@DEF@\n"
+  -c "${filtercond};" \
+  -o ${output} 2>&1 || ldacpass=FALSE 
+} >&1
+
+if [ "${links}" == "TRUE" ] 
+then 
+  rm ${input} ${output}
+  input=${originp}
+  output=${origout}
+fi 
+
+if [ "${ldacpass}" == "TRUE" ]
+then 
+  _message " @BLU@- @RED@Done! (`date +'%a %H:%M'`)@DEF@\n"
+else 
+  _message " @BLU@- @RED@Retry (`date +'%a %H:%M'`)@DEF@\n"
+  _message "@BLU@Filtering with ldactools failed; trying again with @RED@ldac.py@DEF@\n"
+  _message "@BLU@Creating Filtered catalogue for @DEF@${input##*/}@DEF@"
+  #Select sources using required filter condition 
+  @PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/ldacfilter.py \
+    -i ${input} \
+    -t OBJECTS \
+    -c "(@BV:FILTERCOND@);" \
+    -o ${output} 2>&1 
+  _message " @BLU@- @RED@Done! (`date +'%a %H:%M'`)@DEF@\n"
+fi 
 #output=${input//.${ext}/_filt.${ext}}
 _replace_datahead ${input##*/} ${output##*/}
 
