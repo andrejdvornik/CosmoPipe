f16d2d86089b439f4f5d87bb0011dc1b36a3a2bc
Angus H Wright <awright@astro.rub.de>
2023-11-14 22:21:35 +0100

    add equalN_split function

---
+++ scripts/equalN_split.R
@@ -0,0 +1,139 @@
+#=========================================
+#
+# File Name : spatial_split.R
+# Created By : awright
+# Creation Date : 10-07-2023
+# Last Modified : Wed Nov  1 18:48:33 2023
+#
+#=========================================
+
+#Split a catalogue into N equalN sections
+
+#Read input parameters 
+inputs<-commandArgs(TRUE) 
+
+#Interpret the command line options {{{
+badval<- -999
+while (length(inputs)!=0) {
+  #Check for valid specification {{{
+  while (length(inputs)!=0 && inputs[1]=='') { inputs<-inputs[-1] }  
+  if (!grepl('^-',inputs[1])) {
+    print(inputs)
+    stop(paste("Incorrect options provided!"))
+  }
+  #/*fend*/}}}
+  if (inputs[1]=='-i') { 
+    #Read the input catalogue /*fold*/ {{{
+    inputs<-inputs[-1]
+    input.cat<-inputs[1]
+    inputs<-inputs[-1]
+    #/*fold*/}}}
+  } else if (inputs[1]=='-o') { 
+    #Read the output catalogue(s) /*fold*/ {{{
+    inputs<-inputs[-1]
+    if (any(grepl('^-',inputs))) { 
+      output.cats<-inputs[1:(which(grepl('^-',inputs))[1]-1)]
+      inputs<-inputs[-(1:(which(grepl('^-',inputs))[1]-1))]
+    } else { 
+      output.cats<-inputs
+      inputs<-NULL
+    } 
+    #/*fold*/}}}
+  } else if (inputs[1]=='-n') { 
+    #Read the number of splits /*fold*/ {{{
+    inputs<-inputs[-1]
+    nsplit<-as.integer(inputs[1])
+    if (!is.finite(nsplit)) { 
+      stop(paste("Provided number of splits is not a finite integer:",inputs[1]))
+    }
+    inputs<-inputs[-1]
+    #/*fold*/}}}
+  } else if (inputs[1]=='-v') { 
+    #Read the spatial column names /*fold*/ {{{
+    inputs<-inputs[-1]
+    x.name<-inputs[1]
+    inputs<-inputs[-1]
+    #/*fold*/}}}
+  } else {
+    stop(paste("Unknown option",inputs[1]))
+  }
+}
+#}}}
+
+#Check that length of the output files is correct {{{
+if (length(output.cats)!=nsplit) { 
+  stop("Output file list must be of length nsplit") 
+} 
+#}}}
+
+#Read the input catalogue {{{
+cat<-helpRfuncs::read.file(input.cat)
+#}}}
+
+#Check that the x.name varaible is in the catalogue 
+if (!x.name %in% colnames(cat)) {  
+  stop(paste(x.name,"variable is not in provided catalogue!")) 
+}
+
+#Set up the cut object (for faster splitting) {{{
+tmp<-data.frame(x=cat[[x.name]])
+#}}}
+
+#X range {{{
+if (any(tmp$x==badval)) { 
+  cat("WARNING: Catalogue has x-values which are bad!\n")
+  x.range<-range(tmp$x[which(tmp$x!=badval)],na.rm=T)
+} else { 
+  x.range<-range(tmp$x,na.rm=T)
+} 
+#}}}
+
+cat(paste0("x.range: ",'[',x.range[1],',',x.range[2],']\n'))
+
+#Create bins {{{
+if (nsplit==1) { 
+  #There is only one bin?! {{{
+  cat('Only one bin!\n')
+  bins<-rep(1,nrow(tmp))
+  #}}}
+} else {
+  #Bin in x & y {{{
+  if (any(tmp$x==badval)) { 
+    cat('binning in x (with good values only)!\nEntries with bad x values are randomly assigned to a bin!')
+    bins<-rep(NA,nrow(tmp)) 
+    good<-tmp$x!=badval
+    bins[good]<-with(tmp[good,],tapply(1:length(x),list(x=cut(x, breaks=quantile(x,probs=seq(0,1,length=nsplit+1)), include.lowest=T))))
+    bins[!good]<-sample(1:nsplit,size=length(which(!good)),replace=T)
+  } else { 
+    cat('binning in x\n')
+    bins<-with(tmp,tapply(1:nrow(tmp),list(x=cut(x, breaks=quantile(x,probs=seq(0,1,length=nsplit+1)), include.lowest=T))))
+  } 
+  #}}}
+}
+#}}}
+
+cat('bin occupation:\n')
+print(table(bins))
+
+#For each split, output the catalogue {{{
+written<-FALSE
+for (i in seq(1,nsplit)) { 
+  #Select the relevant sources {{{
+  out<-cat[which(bins==i),]
+  #}}}
+  if (nrow(out)==0) { 
+    cat(paste("WARNING: split",i,"contains no sources?!\n"))
+  } else { 
+    #Write the file {{{
+    helpRfuncs::write.file(file=output.cats[i],out)
+    #}}}
+    written<-TRUE
+  }
+} 
+if (!written) { 
+  stop("Nothing was written to disk?!") 
+}
+#}}}
+
+#Finish
+
---
+++ scripts/equalN_split.sh
@@ -0,0 +1,33 @@
+#=========================================
+#
+# File Name : equalN_split.sh
+# Created By : awright
+# Creation Date : 04-07-2023
+# Last Modified : Mon 30 Oct 2023 09:59:43 AM CET
+#
+#=========================================
+
+#Define the output filename 
+input="@DB:DATAHEAD@"
+output=${input}
+outext=${output##*.}
+outbase=${output//.${outext}/}
+#Construct the list of output names 
+outlist=''
+outlist_trunc=''
+for i in `seq @BV:NSPLIT@`
+do 
+  outlist="${outlist} ${outbase}_${i}.${outext}"
+  outlist_trunc="${outlist_trunc} ${outbase##*/}_${i}.${outext}"
+done 
+
+#Split catalogues in the DATAHEAD into NSPLIT regions 
+@P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/equalN_split.R \
+  -i @DB:DATAHEAD@ \
+  -n @BV:NSPLIT@ \
+  -v @BV:SPLITVAR@ \
+  -o ${outlist} 2>&1
+
+#Update the datahead 
+_replace_datahead "${input##*/}" "${outlist_trunc}"
+
