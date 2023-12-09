15e08395a360a4e02217494e7c69cc7455ec4a19
Angus H Wright <awright@astro.rub.de>
2023-12-09 22:07:29 +0100

    add maxlike option to single threaded run switch

--- scripts/run_chain.sh
+++ scripts/run_chain.sh
@@ -3,7 +3,7 @@
 # File Name : run_chain.sh
 # Created By : awright
 # Creation Date : 14-04-2023
-# Last Modified : Thu 07 Sep 2023 06:27:49 PM UTC
+# Last Modified : Sat 09 Dec 2023 10:06:57 PM CET
 #
 #=========================================
 
@@ -12,7 +12,7 @@ ppython=@PYTHON3BIN@
 pythonbin=${ppython%/*}
 _message " >@BLU@ Running cosmosis chain!\n   Start time:@DEF@ `date +'%a %H:%M'`@BLU@)\n@DEF@"
 _message " >@BLU@ Status can be monitored in the logfile located here:\n@RED@ `ls -tr @RUNROOT@/@LOGPATH@/step_*_run_chain.log | tail -n 1` @DEF@\n"
-if [ @BV:NTHREADS@ -eq 1 ] || [ "@BV:SAMPLER@" == "test" ]
+if [ @BV:NTHREADS@ -eq 1 ] || [ "@BV:SAMPLER@" == "test" ] || [ "@BV:SAMPLER@" == "maxlike" ]
 then
 MKL_NUM_THREADS=1 NUMEXPR_NUM_THREADS=1 OMP_NUM_THREADS=1 ${pythonbin}/cosmosis @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/cosmosis_inputs/@SURVEY@_CosmoPipe_constructed_@BV:STATISTIC@.ini 2>&1
 else
