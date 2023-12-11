63d8fa77f2a3f11aa670ccac98f522c9e831482e
BStoelzner <stoelzner@astro.rub.de>
2023-12-11 14:21:50 +0000

    fix filename bug in datablock

--- scripts/add_mcmc_input.sh
+++ scripts/add_mcmc_input.sh
@@ -14,11 +14,24 @@ then
   mkdir @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mcmc_inp_@BV:STATISTIC@/
 fi 
 
-file="@MCMCINPUTFILE@"
-file=${file##*/}
+#Statistic
+BOLTZMAN="@BV:BOLTZMAN@"
+if [ "${BOLTZMAN^^}" == "COSMOPOWER_HM2020" ] || [ "${BOLTZMAN^^}" == "CAMB_HM2020" ]
+then
+  non_linear_model=mead2020_feedback
+elif [ "${BOLTZMAN^^}" == "COSMOPOWER_HM2015" ] || [ "${BOLTZMAN^^}" == "COSMOPOWER_HM2015_S8" ] || [ "${BOLTZMAN^^}" == "CAMB_HM2015" ]
+then
+  non_linear_model=mead2015
+else
+  _message "Boltzmann code not implemented: ${BOLTZMAN^^}\n"
+  exit 1
+fi
+
+file="@MCMCINPUTFILE@/mcmc_inp_@BV:STATISTIC@/MCMC_input_${non_linear_model}.fits"
+filename=${file##*/}
 
 #Create the uncertainty file 
-cp @MCMCINPUTFILE@ @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mcmc_inp_@BV:STATISTIC@/${file}
+cp $file @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mcmc_inp_@BV:STATISTIC@/${filename}
 
 #Update the datablock contents file 
-_write_datablock "mcmc_inp_@BV:STATISTIC@" "${file}"
+_write_datablock "mcmc_inp_@BV:STATISTIC@" "${filename}"
