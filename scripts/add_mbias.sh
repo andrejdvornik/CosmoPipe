7f1bc8d65edf39370b8973ed823a972139126897
Angus H Wright <awright@astro.rub.de>
2024-01-11 00:17:11 +0100

    update add_mbias to use new mbias folder structure

--- scripts/add_mbias.sh
+++ scripts/add_mbias.sh
@@ -3,33 +3,33 @@
 # File Name : add_mbias.sh
 # Created By : awright
 # Creation Date : 30-03-2023
-# Last Modified : Thu 01 Jun 2023 05:50:35 PM CEST
+# Last Modified : Thu 11 Jan 2024 12:16:45 AM CET
 #
 #=========================================
 
 
-#Construct the output directory 
-if [ ! -d @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mbias ]
-then 
-  mkdir @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mbias
-fi 
-
-#Initialise the output list 
-outputlist=""
 #Loop over patches & allpatch 
 for patch in @PATCHLIST@ @ALLPATCH@ 
 do 
+
+  #Construct the output directory 
+  if [ ! -d @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mbias_${patch}_@BV:BLIND@ ]
+  then 
+    mkdir @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mbias_${patch}_@BV:BLIND@ 
+  fi 
+
   #Create the values file 
-  echo "@BV:MBIASVALUES@" > @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mbias/m_${patch}_biases.txt 
+  echo "@BV:MBIASVALUES@" > @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mbias_${patch}_@BV:BLIND@/m_${patch}_biases.txt 
   #Create the uncertainty file 
-  echo "@BV:MBIASERRORS@" > @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mbias/m_${patch}_uncertainty.txt 
+  echo "@BV:MBIASERRORS@" > @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mbias_${patch}_@BV:BLIND@/m_${patch}_uncertainty.txt 
   #Create the correlation file 
-  echo "@BV:MBIASCORR@" > @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mbias/m_${patch}_correlation.txt 
+  echo "@BV:MBIASCORR@" > @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mbias_${patch}_@BV:BLIND@/m_${patch}_correlation.txt 
 
   #update the output list 
-  outputlist="${outputlist} m_${patch}_biases.txt m_${patch}_uncertainty.txt m_${patch}_correlation.txt"
-done 
+  outputlist="m_${patch}_biases.txt m_${patch}_uncertainty.txt m_${patch}_correlation.txt"
 
-#Update the datablock contents file 
-_write_datablock mbias "${outputlist}"
+  #Update the datablock contents file 
+  _write_datablock mbias_${patch}_@BV:BLIND@ "${outputlist}"
+
+done 
 
