838c3a575a6bd915ecb97551160a01222b397ceb
Angus H Wright <awright@astro.rub.de>
2024-02-24 09:18:13 +0000

    make S8-IA plot as well by default

--- scripts/plot_Om_S8.sh
+++ scripts/plot_Om_S8.sh
@@ -3,7 +3,7 @@
 # File Name : plot_Om_S8.sh
 # Created By : awright
 # Creation Date : 04-05-2023
-# Last Modified : Sun 26 Nov 2023 08:26:22 PM CET
+# Last Modified : Sat Feb 24 09:17:52 2024
 #
 #=========================================
 
@@ -18,7 +18,17 @@ fi
   --input @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_@BV:SAMPLER@_@BV:BLIND@.txt \
   --refr @BV:REFCHAIN@ \
   --prior @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_apriori_@BV:BLIND@.txt \
-  --output @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/plots/Om_S8_@BV:SAMPLER@_@BV:BLIND@.png \
+  --output @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/plots/Om_S8_@BV:SAMPLER@_@BV:BLIND@_@BV:STATISTIC@@BV:CHAINSUFFIX@.png \
   --sampler @BV:SAMPLER@ \
+  --title "@BV:SAMPLER@, Blind @BV:BLIND@, @BV:BOLTZMAN@" 2>&1 || echo "ignore failed plot generation" 
+
+#Plot the chain 
+@P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/plot_Om_S8_one.R \
+  --input @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_@BV:SAMPLER@_@BV:BLIND@.txt \
+  --refr @BV:REFCHAIN@ \
+  --prior @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_apriori_@BV:BLIND@.txt \
+  --output @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/plots/S8_IA_@BV:SAMPLER@_@BV:BLIND@@BV:CHAINSUFFIX@.png \
+  --sampler @BV:SAMPLER@ \
+  --xlabel IA_a --xlim -3 3 \
   --title "@BV:SAMPLER@, Blind @BV:BLIND@, @BV:BOLTZMAN@" 2>&1 || echo "ignore failed plot generation" 
 
