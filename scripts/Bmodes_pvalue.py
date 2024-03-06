129c8031d735045150187600d62cc7560866f14e
BStoelzner <stoelzner@astro.rub.de>
2024-03-06 13:26:07 +0000

    compute cosebis pvalues for nmax=5

--- scripts/Bmodes_pvalue.py
+++ scripts/Bmodes_pvalue.py
@@ -70,10 +70,6 @@ for bin1 in range(ntomo):
         else:
             ax[x,y].text(0.03, 0.04, 'p = %.2e'%p, horizontalalignment='left', verticalalignment='bottom', transform = ax[x,y].transAxes)
         bincount+=1
-# ax[0,0].set_xticks((5,10,15,20))
-# ax[0,0].set_ylim((-6,12))
-# ax[0,0].set_ylim((-20,20))
-# ax[0,0].set_yticks((0,20))
 if statistic == 'bandpowers':
     ax[0,0].set_xscale('log')
 fig.supylabel(ylabel)
@@ -85,6 +81,43 @@ p = stats.chi2.sf(chi2, n_data)
 plt.text(0.90, 0.9, 'p = %.2e'%p, fontsize=14, transform=plt.gcf().transFigure, color='black', horizontalalignment='right')
 plt.savefig(output_dir+'/bmodes_%.2f-%.2f.pdf'%(thetamin,thetamax))
 plt.close()
+
+if statistic =='cosebis':
+    fig, ax = plt.subplots(3,7, figsize = (13,5), sharex=True, sharey=True)
+    plt.subplots_adjust(wspace=0, hspace=0, bottom=0.1, left=0.07)
+    bincount=0
+    leg1=Rectangle((0,0),0,0,alpha=0.0)
+    for bin1 in range(ntomo):
+        for bin2 in range(bin1,ntomo):
+            x = bincount//7
+            y = bincount%7
+            idx = np.where((B_data['BIN1']==bin1+1) & (B_data['BIN2']==bin2+1) & (B_data['ANGBIN']<=5))[0]
+            if statistic == 'cosebis':
+                ax[x,y].errorbar(B_data['ANG'][idx], B_data['VALUE'][idx]*1e10, B_std[idx]*1e10, linestyle = 'None', marker = '.', markersize=5)
+            if statistic == 'bandpowers':
+                ax[x,y].errorbar(B_data['ANG'][idx], B_data['VALUE'][idx]/B_data['ANG'][idx]*1e7, B_std[idx]/B_data['ANG'][idx]*1e7, linestyle = 'None', marker = '.', markersize=5)
+            ax[x,y].text(0.03, 0.96, 'zbin %d-%d'%(bin1+1,bin2+1), horizontalalignment='left', verticalalignment='top', transform = ax[x,y].transAxes)
+            ax[x,y].axhline(y=0, color='black', linestyle= 'dashed')
+            chi2 = np.dot(B_data['VALUE'][idx],np.dot(np.linalg.inv(B_cov[idx,:][:,idx]),B_data['VALUE'][idx]))
+            p = stats.chi2.sf(chi2, n_data_per_bin)
+            if p > 1e-2:
+                ax[x,y].text(0.03, 0.04, 'p = %.2f'%p, horizontalalignment='left', verticalalignment='bottom', transform = ax[x,y].transAxes)
+            else:
+                ax[x,y].text(0.03, 0.04, 'p = %.2e'%p, horizontalalignment='left', verticalalignment='bottom', transform = ax[x,y].transAxes)
+            bincount+=1
+    if statistic == 'bandpowers':
+        ax[0,0].set_xscale('log')
+    fig.supylabel(ylabel)
+    fig.supxlabel(xlabel)
+    plt.text(0.07, 0.9, 'Bmodes ' + statistic, fontsize=14, transform=plt.gcf().transFigure, color='red')
+    idx = np.where(B_data['ANGBIN']<=5)[0]
+    chi2 = np.dot(B_data['VALUE'][idx],np.dot(np.linalg.inv(B_cov[idx,:][:,idx]),B_data['VALUE'][idx]))
+    p = stats.chi2.sf(chi2, n_data)
+
+    plt.text(0.90, 0.9, 'p = %.2e'%p, fontsize=14, transform=plt.gcf().transFigure, color='black', horizontalalignment='right')
+    plt.savefig(output_dir+'/bmodes_%.2f-%.2f_nmax5.pdf'%(thetamin,thetamax))
+    plt.close()
+
 
 
 
