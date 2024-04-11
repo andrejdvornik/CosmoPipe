87a3ba5e8414a627fb7f3a2cd76526b86b53cb97
BStoelzner <stoelzner@astro.rub.de>
2024-04-11 07:47:15 +0000

    fix for BP and other things

--- scripts/plot_data_vector_python.py
+++ scripts/plot_data_vector_python.py
@@ -81,6 +81,11 @@ if (statistic == 'xiE') or (statistic == 'xiB'):
     ymin_E = np.min(E_data['ANG']*E_data['VALUE']*scaling*1.3)
     ymax_B = np.max(B_data['ANG']*B_data['VALUE']*scaling*1.3)
     ymin_B = np.min(B_data['ANG']*B_data['VALUE']*scaling*1.3)
+elif statistic == 'bandpowers':
+    ymax_E = np.max(E_data['VALUE']/E_data['ANG']*scaling*1.3)
+    ymin_E = np.min(E_data['VALUE']/E_data['ANG']*scaling*1.3)
+    ymax_B = np.max(B_data['VALUE']/B_data['ANG']*scaling*1.3)
+    ymin_B = np.min(B_data['VALUE']/B_data['ANG']*scaling*1.3)
 else:
     ymax_E = np.max(E_data['VALUE']*scaling*1.3)
     ymin_E = np.min(E_data['VALUE']*scaling*1.3)
@@ -118,7 +123,7 @@ pl.clf()
 fig = pl.figure(1, figsize = (20,20))
 pl.clf()
 
-if statistic == 'cosebis':
+if (statistic == 'cosebis') or (statistic == 'bandpowers'):
     # Plotting
     for bin1 in range(1,ntomo+1):
         for bin2 in range(bin1,ntomo+1):
@@ -128,7 +133,10 @@ if statistic == 'cosebis':
             pl.xscale(xscale)
             idx = np.where((E_data['BIN1']==bin1) & (E_data['BIN2']==bin2))[0]
             # Plot data
-            ax.errorbar(E_data['ANG'][idx],E_data['VALUE'][idx]*scaling, yerr=E_std[idx]*scaling, fmt='d',markeredgecolor='C0',mew=1,markerfacecolor='C0', color='C0', markersize=4)
+            if statistic == 'bandpowers':
+                ax.errorbar(E_data['ANG'][idx], E_data['VALUE'][idx]/E_data['ANG'][idx]*scaling, E_std[idx]/E_data['ANG'][idx]*scaling, linestyle = 'None', marker = '.', markersize=5)
+            else:
+                ax.errorbar(E_data['ANG'][idx],E_data['VALUE'][idx]*scaling, yerr=E_std[idx]*scaling, fmt='d',markeredgecolor='C0',mew=1,markerfacecolor='C0', color='C0', markersize=4)
 
             ax.axhline(y=0, color='k', ls=':',label='')
             pl.setp(ax.get_yticklabels(),  visible=False)
@@ -177,7 +185,10 @@ if statistic == 'cosebis':
                 ax.set_xlabel(xlabel)
             idx = np.where((E_data['BIN1']==bin1) & (E_data['BIN2']==bin2))[0]
             # Plot data
-            ax.errorbar(B_data['ANG'][idx],B_data['VALUE'][idx]*scaling, yerr=B_std[idx]*scaling, fmt='d',markeredgecolor='C0',mew=1,markerfacecolor='C0', color='C0', markersize=4)
+            if statistic == 'bandpowers':
+                ax.errorbar(B_data['ANG'][idx], B_data['VALUE'][idx]/B_data['ANG'][idx]*scaling, B_std[idx]/B_data['ANG'][idx]*scaling, linestyle = 'None', marker = '.', markersize=5)
+            else:
+                ax.errorbar(B_data['ANG'][idx],B_data['VALUE'][idx]*scaling, yerr=B_std[idx]*scaling, fmt='d',markeredgecolor='C0',mew=1,markerfacecolor='C0', color='C0', markersize=4)
             
             ax.axhline(y=0, color='k', ls=':')
             lg = pl.legend([leg1],['z-'+str(bin1)+str(bin2)],loc='upper right',
@@ -196,107 +207,108 @@ if statistic == 'cosebis':
         pl.savefig(output_dir+'/datavector_%.2f-%.2f.pdf'%(thetamin,thetamax))
     pl.close()
 
-    yprops=dict(rotation=90, horizontalalignment='center',verticalalignment='center', x=10,labelpad=20, fontsize=15)
-    leg1=Rectangle((0,0),0,0,alpha=0.0)
-    formatter = ScalarFormatter(useMathText=True)
-    formatter.set_scientific(True)                                                                                               
-    fig_width_pt = 246.0*3.5 # Get this from LaTex using \the\columnwidth  
-    inches_per_pt = 1.0/72.27
-    golden_mean = (np.sqrt(5)-1.0)/2.0
-    fig_width  = fig_width_pt*inches_per_pt # width in inches                                                                      
-    fig_height = fig_width*golden_mean # height in inches                                                                          
-    fig_size = [fig_width, fig_height*1.5]           
-    fontsize=15
-    params = {'axes.labelsize':15,
-            'font.size':10,
-            'legend.fontsize':17,
-            'xtick.labelsize':12,
-            'ytick.labelsize':12,
-            'figure.figsize':fig_size,
-            'font.family': 'serif'}
-    pl.rcParams.update(params)
-    pl.subplots_adjust(wspace=0.,hspace=0.) # you can add spaces between the block here
-    pl.clf()
-    fig = pl.figure(1, figsize = (20,20))
-    pl.clf()
-    # Plotting
-    for bin1 in range(1,ntomo+1):
-        for bin2 in range(bin1,ntomo+1):
-            index=int(ntomo*(bin1-1)-(bin1-1)*(bin1-1-1)/2+(bin2-1)-(bin1-1))
-            ax=pl.subplot(ntomo+1,ntomo+1,(bin1-1)*(ntomo+1)+(bin2-1)+2) # use this for upper triangle
-            ax.set_ylim(ymin_E,ymax_E)
-            pl.xscale(xscale)
-            idx = np.where((E_data['BIN1']==bin1) & (E_data['BIN2']==bin2) & (E_data['ANGBIN']<=5))[0]
-            # Plot data
-            ax.errorbar(E_data['ANG'][idx],E_data['VALUE'][idx]*scaling, yerr=E_std[idx]*scaling, fmt='d',markeredgecolor='C0',mew=1,markerfacecolor='C0', color='C0', markersize=4)
+    if statistic == 'cosebis':
+        yprops=dict(rotation=90, horizontalalignment='center',verticalalignment='center', x=10,labelpad=20, fontsize=15)
+        leg1=Rectangle((0,0),0,0,alpha=0.0)
+        formatter = ScalarFormatter(useMathText=True)
+        formatter.set_scientific(True)                                                                                               
+        fig_width_pt = 246.0*3.5 # Get this from LaTex using \the\columnwidth  
+        inches_per_pt = 1.0/72.27
+        golden_mean = (np.sqrt(5)-1.0)/2.0
+        fig_width  = fig_width_pt*inches_per_pt # width in inches                                                                      
+        fig_height = fig_width*golden_mean # height in inches                                                                          
+        fig_size = [fig_width, fig_height*1.5]           
+        fontsize=15
+        params = {'axes.labelsize':15,
+                'font.size':10,
+                'legend.fontsize':17,
+                'xtick.labelsize':12,
+                'ytick.labelsize':12,
+                'figure.figsize':fig_size,
+                'font.family': 'serif'}
+        pl.rcParams.update(params)
+        pl.subplots_adjust(wspace=0.,hspace=0.) # you can add spaces between the block here
+        pl.clf()
+        fig = pl.figure(1, figsize = (20,20))
+        pl.clf()
+        # Plotting
+        for bin1 in range(1,ntomo+1):
+            for bin2 in range(bin1,ntomo+1):
+                index=int(ntomo*(bin1-1)-(bin1-1)*(bin1-1-1)/2+(bin2-1)-(bin1-1))
+                ax=pl.subplot(ntomo+1,ntomo+1,(bin1-1)*(ntomo+1)+(bin2-1)+2) # use this for upper triangle
+                ax.set_ylim(ymin_E,ymax_E)
+                pl.xscale(xscale)
+                idx = np.where((E_data['BIN1']==bin1) & (E_data['BIN2']==bin2) & (E_data['ANGBIN']<=5))[0]
+                # Plot data
+                ax.errorbar(E_data['ANG'][idx],E_data['VALUE'][idx]*scaling, yerr=E_std[idx]*scaling, fmt='d',markeredgecolor='C0',mew=1,markerfacecolor='C0', color='C0', markersize=4)
 
-            ax.axhline(y=0, color='k', ls=':',label='')
-            pl.setp(ax.get_yticklabels(),  visible=False)
-            pl.setp(ax.get_xticklabels(),  visible=False)
-            if bin1==1:
-                ax.set_xlabel(xlabel)
-                ax.xaxis.set_label_coords(0.5, 1.4) 
-                pl.setp(ax.get_xticklabels(),  visible=True)
-                ax.xaxis.tick_top()
-            if bin2==ntomo:
-                pl.setp(ax.get_yticklabels(),  visible=True)
-                ax.yaxis.tick_right()
-            if (bin2==ntomo) & (bin1==np.floor(ntomo/2.)):
-                ax.set_ylabel(ylabel_E,**yprops)
-                ax.yaxis.set_label_coords(1.4, 0.0) 
-            lg = pl.legend([leg1],['z-'+str(bin1)+str(bin2)],loc='upper right',
-                        handlelength=0,borderpad=0,labelspacing=0.,ncol=3,prop={'size':fontsize}
-                        ,columnspacing=0,frameon=None)
-            lg.draw_frame(False)
-            # pl.gca().add_artist(lg)
-            # if((bin1==1) & (bin2==bin1)):
-            #     lg = pl.legend(bbox_to_anchor=(0.0, 1),handlelength=1,borderpad=0,labelspacing=0.1,ncol=1,prop={'size':fontsize}
-            #         ,columnspacing=0,frameon=None)
-            #     lg.draw_frame(False)
-                
-            #################################################################
-            # lower triangle for B-modes
-            ax=pl.subplot(ntomo+1,ntomo+1,(bin2-1)*(ntomo+1)+(bin1-1)+ntomo+2)
-            if bin1==1:
-                pl.setp(ax.get_yticklabels(),  visible=True)
-            else:
+                ax.axhline(y=0, color='k', ls=':',label='')
                 pl.setp(ax.get_yticklabels(),  visible=False)
-            if bin2==ntomo:
-                pl.setp(ax.get_xticklabels(),  visible=True)
-            else:
                 pl.setp(ax.get_xticklabels(),  visible=False)
+                if bin1==1:
+                    ax.set_xlabel(xlabel)
+                    ax.xaxis.set_label_coords(0.5, 1.4) 
+                    pl.setp(ax.get_xticklabels(),  visible=True)
+                    ax.xaxis.tick_top()
+                if bin2==ntomo:
+                    pl.setp(ax.get_yticklabels(),  visible=True)
+                    ax.yaxis.tick_right()
+                if (bin2==ntomo) & (bin1==np.floor(ntomo/2.)):
+                    ax.set_ylabel(ylabel_E,**yprops)
+                    ax.yaxis.set_label_coords(1.4, 0.0) 
+                lg = pl.legend([leg1],['z-'+str(bin1)+str(bin2)],loc='upper right',
+                            handlelength=0,borderpad=0,labelspacing=0.,ncol=3,prop={'size':fontsize}
+                            ,columnspacing=0,frameon=None)
+                lg.draw_frame(False)
+                # pl.gca().add_artist(lg)
+                # if((bin1==1) & (bin2==bin1)):
+                #     lg = pl.legend(bbox_to_anchor=(0.0, 1),handlelength=1,borderpad=0,labelspacing=0.1,ncol=1,prop={'size':fontsize}
+                #         ,columnspacing=0,frameon=None)
+                #     lg.draw_frame(False)
+                    
+                #################################################################
+                # lower triangle for B-modes
+                ax=pl.subplot(ntomo+1,ntomo+1,(bin2-1)*(ntomo+1)+(bin1-1)+ntomo+2)
+                if bin1==1:
+                    pl.setp(ax.get_yticklabels(),  visible=True)
+                else:
+                    pl.setp(ax.get_yticklabels(),  visible=False)
+                if bin2==ntomo:
+                    pl.setp(ax.get_xticklabels(),  visible=True)
+                else:
+                    pl.setp(ax.get_xticklabels(),  visible=False)
+                    
+                # ax.set_ylim(ymin_B,ymax_B)
+                ax.set_ylim(ymin_E,ymax_E)
+                pl.xscale(xscale)
                 
-            # ax.set_ylim(ymin_B,ymax_B)
-            ax.set_ylim(ymin_E,ymax_E)
-            pl.xscale(xscale)
-            
-            if (bin1==1)&(bin2==np.floor(ntomo/2.)):
-                ax.set_ylabel(ylabel_B,**yprops)
-                ax.yaxis.set_label_coords(-0.5, 0.0) 
-            if bin2==ntomo:
-                ax.set_xlabel(xlabel)
-            idx = np.where((E_data['BIN1']==bin1) & (E_data['BIN2']==bin2) & (B_data['ANGBIN']<=5))[0]
-            # Plot data
-            ax.errorbar(B_data['ANG'][idx],B_data['VALUE'][idx]*scaling, yerr=B_std[idx]*scaling, fmt='d',markeredgecolor='C0',mew=1,markerfacecolor='C0', color='C0', markersize=4)
-            
-            ax.axhline(y=0, color='k', ls=':')
-            lg = pl.legend([leg1],['z-'+str(bin1)+str(bin2)],loc='upper right',
-                    handlelength=0,borderpad=0,labelspacing=0.1,ncol=2,prop={'size':fontsize}
-                    ,columnspacing=0,frameon=None)
-            lg.draw_frame(False)
-            # pl.gca().add_artist(lg)
-            # if((bin1==1) & (bin2==bin1)):
-            #     lg = pl.legend(bbox_to_anchor=(-0.1, 0.5),handlelength=1,borderpad=0,labelspacing=0.1,ncol=1,prop={'size':fontsize}
-            #         ,columnspacing=0,frameon=None)
-            #     lg.draw_frame(False)
-    fig.suptitle(r'%s %s %s, $\theta=[%.2f,%.2f]$'%(title,suffix,statistic,thetamin,thetamax), fontsize=20)
-    if suffix:
-        pl.savefig(output_dir+'/datavector_%.2f-%.2f_%s_nmax5.pdf'%(thetamin,thetamax,suffix))
-    else:
-        pl.savefig(output_dir+'/datavector_%.2f-%.2f_nmax5.pdf'%(thetamin,thetamax))
-    pl.close()
+                if (bin1==1)&(bin2==np.floor(ntomo/2.)):
+                    ax.set_ylabel(ylabel_B,**yprops)
+                    ax.yaxis.set_label_coords(-0.5, 0.0) 
+                if bin2==ntomo:
+                    ax.set_xlabel(xlabel)
+                idx = np.where((E_data['BIN1']==bin1) & (E_data['BIN2']==bin2) & (B_data['ANGBIN']<=5))[0]
+                # Plot data
+                ax.errorbar(B_data['ANG'][idx],B_data['VALUE'][idx]*scaling, yerr=B_std[idx]*scaling, fmt='d',markeredgecolor='C0',mew=1,markerfacecolor='C0', color='C0', markersize=4)
+                
+                ax.axhline(y=0, color='k', ls=':')
+                lg = pl.legend([leg1],['z-'+str(bin1)+str(bin2)],loc='upper right',
+                        handlelength=0,borderpad=0,labelspacing=0.1,ncol=2,prop={'size':fontsize}
+                        ,columnspacing=0,frameon=None)
+                lg.draw_frame(False)
+                # pl.gca().add_artist(lg)
+                # if((bin1==1) & (bin2==bin1)):
+                #     lg = pl.legend(bbox_to_anchor=(-0.1, 0.5),handlelength=1,borderpad=0,labelspacing=0.1,ncol=1,prop={'size':fontsize}
+                #         ,columnspacing=0,frameon=None)
+                #     lg.draw_frame(False)
+        fig.suptitle(r'%s %s %s, $\theta=[%.2f,%.2f]$'%(title,suffix,statistic,thetamin,thetamax), fontsize=20)
+        if suffix:
+            pl.savefig(output_dir+'/datavector_%.2f-%.2f_%s_nmax5.pdf'%(thetamin,thetamax,suffix))
+        else:
+            pl.savefig(output_dir+'/datavector_%.2f-%.2f_nmax5.pdf'%(thetamin,thetamax))
+        pl.close()
 
-    pl.rcParams.update(pl.rcParamsDefault)
+        pl.rcParams.update(pl.rcParamsDefault)
 
 if (statistic == 'xiE') or (statistic == 'xiB') or (statistic == 'xipm'):
     # Plotting
