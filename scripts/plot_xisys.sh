8a12be476dae146bd8a04a7bb74269113eed9714
Angus H Wright <awright@astro.rub.de>
2024-04-18 12:47:40 +0200

    plot multiple statistics in xisys function

--- scripts/plot_xisys.sh
+++ scripts/plot_xisys.sh
@@ -3,7 +3,7 @@
 # File Name : plot_xisys.sh
 # Created By : awright
 # Creation Date : 29-02-2024
-# Last Modified : Fri 01 Mar 2024 09:36:58 AM CET
+# Last Modified : Thu 04 Apr 2024 12:51:42 AM CEST
 #
 #=========================================
 
@@ -29,17 +29,44 @@ covariance=`echo ${covariance} |  awk '{print $1}'`
 
 NTOMO=`echo @BV:TOMOLIMS@ | awk '{print NF-1}'`
 
-for ptype in upper lower 
-do 
-  getlab="ylabel_${ptype}"
-  #Run the R plotting code 
-  @P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/plot_xisys.R \
-    --xipmvec ${xipmvec} \
-    --xipsfvec ${xipsfvec} \
-    --xigpsfvec ${xigpsfvec} \
-    --xipm_tpd @RUNROOT@/@STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_list_@BV:LIST_INPUT_SAMPLER@_@BV:BLIND@.txt \
-    --covariance ${covariance} \
-    --ntomo ${NTOMO} \
-    --output @RUNROOT@/@STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/plots/Xi_Sys_@BV:LIST_INPUT_SAMPLER@_@BV:BLIND@.pdf \
-    2>&1
-done 
+#Run the R plotting code 
+@P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/plot_xipsf.R \
+  --xipmvec ${xipmvec} \
+  --xipsfvec ${xipsfvec} \
+  --xigpsfvec ${xigpsfvec} \
+  --xipm_tpd @RUNROOT@/@STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_list_@BV:LIST_INPUT_SAMPLER@_@BV:BLIND@.txt \
+  --covariance ${covariance} \
+  --ntomo ${NTOMO} \
+  --nmax @BV:NXIPM@ \
+  --thetamin @BV:THETAMINXI@ \
+  --thetamax @BV:THETAMAXXI@ \
+  --output @RUNROOT@/@STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/plots/Xi_PSF_@BV:LIST_INPUT_SAMPLER@_@BV:BLIND@.pdf \
+  2>&1
+#Run the R plotting code 
+@P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/plot_xigpsf.R \
+  --xipmvec ${xipmvec} \
+  --xipsfvec ${xipsfvec} \
+  --xigpsfvec ${xigpsfvec} \
+  --xipm_tpd @RUNROOT@/@STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_list_@BV:LIST_INPUT_SAMPLER@_@BV:BLIND@.txt \
+  --covariance ${covariance} \
+  --ntomo ${NTOMO} \
+  --nmax @BV:NXIPM@ \
+  --thetamin @BV:THETAMINXI@ \
+  --thetamax @BV:THETAMAXXI@ \
+  --output @RUNROOT@/@STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/plots/Xi_gPSF_@BV:LIST_INPUT_SAMPLER@_@BV:BLIND@.pdf \
+  2>&1
+#Run the R plotting code 
+@P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/plot_xisys.R \
+  --xipmvec ${xipmvec} \
+  --xipsfvec ${xipsfvec} \
+  --xigpsfvec ${xigpsfvec} \
+  --xipm_tpd @RUNROOT@/@STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_list_@BV:LIST_INPUT_SAMPLER@_@BV:BLIND@.txt \
+  --covariance ${covariance} \
+  --ntomo ${NTOMO} \
+  --nmax @BV:NXIPM@ \
+  --thetamin @BV:THETAMINXI@ \
+  --thetamax @BV:THETAMAXXI@ \
+  --output @RUNROOT@/@STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/plots/Xi_Sys_@BV:LIST_INPUT_SAMPLER@_@BV:BLIND@.pdf \
+  2>&1
+
+
