b8fade17bcd7d28d617b3a8bedc231072770e934
Andrej Dvornik <andrejdvornik@users.noreply.github.com>
2024-04-19 10:30:55 +0200

    Create calc_smf.man.sh

---
+++ man/calc_smf.man.sh
@@ -0,0 +1,79 @@
+#
+# calc_smf.sh Documentation & Housekeeping functions
+#
+
+#Starting Prompt {{{
+function _prompt { 
+  #Check if we do want verbose output
+  if [ "$1" != "0" ] 
+  then
+    _message "@BLU@==========================================@DEF@\n"
+    _message "@BLU@== @RED@ Running calc_smf.sh Mode @BLU@ ==@DEF@\n"
+    _message "@BLU@==========================================@DEF@\n"
+  fi 
+}
+#}}}
+
+#Mode description {{{
+function _description { 
+  echo "#"
+  echo '# Compute stellar mass function for all '
+  echo '# lens catalogues specified in the pipeline/variables.sh '
+  echo "#"
+  echo "# Function takes input data:"
+  echo "# `_inp_data`"
+  echo "#"
+}
+#}}}
+
+# Abort Message {{{
+_abort()
+{
+  #Message to print when script aborts 
+  #$0 is the script that was running when this error occurred
+  _message "@BLU@ An error occured while running:\n@DEF@$0.\n" >&2
+  _message "@BLU@ Check the logging file for this step in:\n" >&2
+  _message "@DEF@@RUNROOT@/@LOGPATH@/\n" >&2
+  exit 1
+}
+trap '_abort' 0
+set -e 
+#}}}
+
+# Input variables {{{ 
+function _inp_var { 
+  #Variable inputs (leave blank if none)
+  echo ALLPATCH BLU BV:NSMFBINS BV:MINMASS BV:MAXMASS BV:H0 BV:OMEGAM BV:OMEGAV BV:MINZ BV:MAXZ BV:REDSHIFT BV:STELLARMASS BV:SURVEYAREADEG DATABLOCK DEF PATCHLIST PYTHON3BIN RED RUNROOT SCRIPTPATH STORAGEPATH
+}
+#}}}
+
+# Input data {{{ 
+function _inp_data { 
+  #Data inputs (leave blank if none)
+  echo SMF_LENS_CATS mass_lims
+}
+#}}}
+
+# Output data {{{ 
+function _outputs { 
+  #Data outputs (leave blank if none)
+  echo smf vmax
+}
+#}}}
+
+# Execution command {{{ 
+function _runcommand { 
+  #Command for running the script 
+  echo bash @RUNROOT@/@SCRIPTPATH@/calc_wt_w_treecorr.sh
+}
+#}}}
+
+# Unset Function command {{{ 
+function _unset_functions { 
+  #Remove these functions from the environment
+  unset -f _prompt _description _inp_data _inp_var _abort _outputs _runcommand _unset_functions
+} 
+#}}}
+
+#Additional Functions 
+
