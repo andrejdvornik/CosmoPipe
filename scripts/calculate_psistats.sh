0746988a1d6b0308271b6df4b58e1ab60db36272
Andrej Dvornik <andrejdvornik@users.noreply.github.com>
2024-06-11 11:47:22 +0200

    Binning variables fixes, fixing comments

--- scripts/calculate_psistats.sh
+++ scripts/calculate_psistats.sh
@@ -30,16 +30,14 @@ then
   # -t = treecorr output theta_col - the first column is zero so -t 1 uses the meanR from Treecorr
   # -g = treecorr output gamma_t
   # -q = treecorr output gamma_x
-  # --cfoldername = output directory
+  # --psifoldername = output directory
   # -o = filename (outputs En_filename.ascii and Bn_filename.ascii)
   # -b = binning "log" or "lin"
   # -nPsi = number of PSI stats modes
   # -s = PSI stats minimum theta
   # -l = PSI stats maximum theta
   # location of the required pre-compution tables
-  # --tfoldername = Tplus_minus    # computes/saves the first time it is run for a fixed theta min/max
-  # --norm = TLogsRootsAndNorms/Normalization_${tmin}-${tmax}.table
-  # --root = TLogsRootsAndNorms/Root_${tmin}-${tmax}.table
+  # --filterfoldername = Tplus_minus    # computes/saves the first time it is run for a fixed theta min/max
   
   _message "    -> @BLU@Computing PSI stats for file @RED@${input##*/}@DEF@"
   @PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/run_measure_statistics_cats2stats.py \
@@ -64,17 +62,15 @@ then
   # Now Integrate output from treecorr with PSI stats filter functions
   # -i = input file
   # -t = treecorr output theta_col - the first column is zero so -t 1 uses the meanR from Treecorr
-  # -l = treecorr output xi
-  # --cfoldername = output directory
+  # -j = treecorr output xi
+  # --psifoldername = output directory
   # -o = filename (outputs En_filename.ascii and Bn_filename.ascii)
   # -b = binning "log" or "lin"
   # -nPsi = number of PSI stats modes
   # -s = PSI stats minimum theta
   # -l = PSI stats maximum theta
   # location of the required pre-compution tables
-  # --tfoldername = Tplus_minus    # computes/saves the first time it is run for a fixed theta min/max
-  # --norm = TLogsRootsAndNorms/Normalization_${tmin}-${tmax}.table
-  # --root = TLogsRootsAndNorms/Root_${tmin}-${tmax}.table
+  # --filterfoldername = Tplus_minus    # computes/saves the first time it is run for a fixed theta min/max
   
   _message "    -> @BLU@Computing PSI stats for file @RED@${input##*/}@DEF@"
   @PYTHON3BIN@ @RUNROOT@/@SCRIPTPATH@/run_measure_statistics_cats2stats.py \
