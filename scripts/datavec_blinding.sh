1f61f8f04f36c61b6345e400e2023fa9f9098fb0
Andrej Dvornik <andrejdvornik@users.noreply.github.com>
2024-08-06 15:48:28 +0200

    Enable removal of original fits file through blinding script

--- scripts/datavec_blinding.sh
+++ scripts/datavec_blinding.sh
@@ -30,7 +30,7 @@ fi
 MKL_NUM_THREADS=1 NUMEXPR_NUM_THREADS=1 OMP_NUM_THREADS=1 @PYTHON3BIN@ -m blind_2pt_cosmosis \
     -i @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/cosmosis_inputs/@SURVEY@_CosmoPipe_constructed_@BV:STATISTIC@.ini \
     -u @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mcmc_inp_@BV:STATISTIC@/MCMC_input_${non_linear_model}.fits \
-    -b \
+    -b -r \
     --key_path @BV:KEYPATH@ \
     --file_path @BV:BLINDFILE@ 2>&1
 
@@ -40,7 +40,7 @@ then
   MKL_NUM_THREADS=1 NUMEXPR_NUM_THREADS=1 OMP_NUM_THREADS=1 @PYTHON3BIN@ -m blind_2pt_cosmosis \
     -i @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/cosmosis_inputs/@SURVEY@_CosmoPipe_constructed_@BV:STATISTIC@.ini \
     -u @RUNROOT@/@STORAGEPATH@/@DATABLOCK@/mcmc_inp_@BV:STATISTIC@/MCMC_input_${non_linear_model}_no_m_bias.fits \
-    -b \
+    -b -r \
     --key_path @BV:KEYPATH@ \
     --file_path @BV:BLINDFILE@ 2>&1
 fi
