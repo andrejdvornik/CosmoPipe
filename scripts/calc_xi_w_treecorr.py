New covariance read-in in the plotting script, small bug fixes in treecorr scripts

scripts/calc_xi_w_treecorr.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
              help='Name of the Dec column in the first catalogue')
     parser.add_argument('--file2dec', dest="cat2decname", type=str,default='DELTA_J2000',
              help='Name of the Dec column in the second catalogue')
-    parser.add_argument('--patch_centers', dest="center_file", type=str, nargs='?',default='',
+    parser.add_argument('--patch_centers', dest="center_file", type=str, nargs='?',default=None,
              help='File containing centers for performing jackknife/bootstrap covariance calculations')
     parser.add_argument('--nthreads', dest="num_threads", type=int,default=None,
              help='Number of desired parallel threads. If None (default) then uses all available')
@@ -222,6 +222,6 @@ if __name__ == '__main__':
         # Write it out unweighted npairs and praise-be again for Jarvis and his well documented code
         gg.write(outfile, precision=12)
 
-    if center_file != "": 
+    if center_file is not None:
         np.savetxt(covoutfile,gg.cov)
 

scripts/plot_data_vector_python.py
@@ -155,20 +155,17 @@ if __name__ == '__main__':
         if nn:
             nn_data = f[extension_nn].data
             n_data_nn = len(nn_data)
-            #nn_cov = f['COVMAT'].data[0:n_data_nn,:][:,0:n_data_nn]
-            nn_cov = np.eye(n_data_nn)
+            nn_cov = f['COVMAT'].data[0:n_data_nn,:][:,0:n_data_nn]
             nn_std = np.sqrt(np.diag(nn_cov))
         if ne:
             neE_data = f[extension_neE].data
             n_data_ne = len(neE_data)
-            #neE_cov = f['COVMAT'].data[n_data_nn:n_data_nn+n_data_ne,:][:,n_data_nn:n_data_nn+n_data_ne]
-            neE_cov = np.eye(n_data_ne)
+            neE_cov = f['COVMAT'].data[n_data_nn:n_data_nn+n_data_ne,:][:,n_data_nn:n_data_nn+n_data_ne]
             neE_std = np.sqrt(np.diag(neE_cov))
             try:
                 neB_data = f[extension_neB].data
                 n_data_nb = len(neB_data)
-                #neB_cov = f['COVMAT'].data[n_data_nn+n_data_ne:n_data_nn+n_data_ne+n_data_nb,:][:,n_data_nn+n_data_ne:n_data_nn+n_data_ne+n_data_nb]
-                neB_cov = np.eye(n_data_ne)
+                neB_cov = f['COVMAT'].data[n_data_nn+n_data_ne:n_data_nn+n_data_ne+n_data_nb,:][:,n_data_nn+n_data_ne:n_data_nn+n_data_ne+n_data_nb]
                 neB_std = np.sqrt(np.diag(neB_cov))
             except:
                 neB_data = None
@@ -177,14 +174,12 @@ if __name__ == '__main__':
         if ee:
             eeE_data = f[extension_eeE].data
             n_data_ee = len(eeE_data)
-            #eeE_cov = f['COVMAT'].data[n_data_nn+n_data_ne+n_data_nb:n_data_nn+n_data_ne+n_data_nb+n_data_ee,:][:,n_data_nn+n_data_ne+n_data_nb:n_data_nn+n_data_ne+n_data_nb+n_data_ee]
-            eeE_cov = np.eye(n_data_ee)
+            eeE_cov = f['COVMAT'].data[n_data_nn+n_data_ne+n_data_nb:n_data_nn+n_data_ne+n_data_nb+n_data_ee,:][:,n_data_nn+n_data_ne+n_data_nb:n_data_nn+n_data_ne+n_data_nb+n_data_ee]
             eeE_std = np.sqrt(np.diag(eeE_cov))
             try:
                 eeB_data = f[extension_eeB].data
                 n_data_bb = len(eeB_data)
-                #eeB_cov = f['COVMAT'].data[n_data_nn+n_data_ne+n_data_nb+n_data_ee:n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb,:][:,n_data_nn+n_data_ne+n_data_nb+n_data_ee:n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb]
-                eeB_cov = np.eye(n_data_ee)
+                eeB_cov = f['COVMAT'].data[n_data_nn+n_data_ne+n_data_nb+n_data_ee:n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb,:][:,n_data_nn+n_data_ne+n_data_nb+n_data_ee:n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb]
                 eeB_std = np.sqrt(np.diag(eeB_cov))
             except:
                 eeB_data = None
@@ -193,8 +188,7 @@ if __name__ == '__main__':
         if obs:
             obs_data = f[extension_obs].data
             n_data_obs = np.sum([len(obs_data[f'ANG{i+1}']) for i in range(nobs)])
-            #nn_cov = f['COVMAT'].data[n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb:n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb+n_data_nn,:][:,n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb:n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb+n_data_nn]
-            obs_cov = np.eye(n_data_obs)
+            obs_cov = f['COVMAT'].data[n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb:n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb+n_data_obs,:][:,n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb:n_data_nn+n_data_ne+n_data_nb+n_data_ee+n_data_nb+n_data_obs]
             obs_std = np.sqrt(np.diag(obs_cov))
 
     