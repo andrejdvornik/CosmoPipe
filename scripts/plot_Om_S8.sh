7d757e2ffca52e82bd632c8eeb4165145d47c071
Angus H Wright <awright@astro.rub.de>
2024-10-30 12:42:15 +0100

    use chainsuffix in plot_Om_S8

--- scripts/plot_Om_S8.sh
+++ scripts/plot_Om_S8.sh
@@ -3,7 +3,7 @@
 # File Name : plot_Om_S8.sh
 # Created By : awright
 # Creation Date : 04-05-2023
-# Last Modified : Sat Feb 24 09:17:52 2024
+# Last Modified : Fri Oct 18 13:01:27 2024
 #
 #=========================================
 
@@ -15,20 +15,21 @@ fi
 
 #Plot the chain 
 @P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/plot_Om_S8_one.R \
-  --input @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_@BV:SAMPLER@_@BV:BLIND@.txt \
+  --input @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_@BV:SAMPLER@_@BV:BLIND@@BV:CHAINSUFFIX@.txt \
   --refr @BV:REFCHAIN@ \
-  --prior @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_apriori_@BV:BLIND@.txt \
+  --prior @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_apriori_@BV:BLIND@@BV:CHAINSUFFIX@.txt \
   --output @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/plots/Om_S8_@BV:SAMPLER@_@BV:BLIND@_@BV:STATISTIC@@BV:CHAINSUFFIX@.png \
   --sampler @BV:SAMPLER@ \
   --title "@BV:SAMPLER@, Blind @BV:BLIND@, @BV:BOLTZMAN@" 2>&1 || echo "ignore failed plot generation" 
 
 #Plot the chain 
 @P_RSCRIPT@ @RUNROOT@/@SCRIPTPATH@/plot_Om_S8_one.R \
-  --input @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_@BV:SAMPLER@_@BV:BLIND@.txt \
+  --input @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_@BV:SAMPLER@_@BV:BLIND@@BV:CHAINSUFFIX@.txt \
   --refr @BV:REFCHAIN@ \
-  --prior @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_apriori_@BV:BLIND@.txt \
+  --prior @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/chain/output_apriori_@BV:BLIND@@BV:CHAINSUFFIX@.txt \
   --output @STORAGEPATH@/MCMC/output/@SURVEY@_@BLINDING@/@BV:BOLTZMAN@/@BV:STATISTIC@/plots/S8_IA_@BV:SAMPLER@_@BV:BLIND@@BV:CHAINSUFFIX@.png \
   --sampler @BV:SAMPLER@ \
-  --xlabel IA_a --xlim -3 3 \
+  --xlabel IA_a --xtitle "'IA amplitude'" --xlim -3 3 \
   --title "@BV:SAMPLER@, Blind @BV:BLIND@, @BV:BOLTZMAN@" 2>&1 || echo "ignore failed plot generation" 
+
 
